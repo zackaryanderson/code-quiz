f21efd4f3b53c7c74366079d599c6474570bf81d
zanderson1998 <zanderson1998@yahoo.com>
2020-11-21 14:36:57 -0700

    inital framework and psudocoding for project

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//load high scores
+
+//add timer *can i use js for specific placements?
+
+//add question arrays and answer arrays *use methods to initalize questions
+
+//add pop ups at bottom of the screen for correct/wrong answers
+
+//save highscores
