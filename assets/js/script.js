dd9ab2447bc332c6a0a64ffb60ec93369c635b94
zanderson1998 <zanderson1998@yahoo.com>
2020-11-22 21:15:13 -0700

    Clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var pageContentEl = document.querySelector("#wrapper");
-//load high scores
+
 //add question arrays and answer arrays *use methods to initalize questions
 var questions = [
     {
@@ -30,20 +30,14 @@ var questions = [
 ];
 
 var highScore = localStorage.getItem("highscore");
-// if (highScore === null) {
-//     highScore = [""];
-// }
-// console.log(highScore);
-// //highScore = JSON.parse(highScore);
+if (highScore === null) {
+    highScore = [""];
+}
 
-var lowScore = localStorage.getItem("lowscore");
-// if (lowScore === null) {
-//     lowScore = [""];
-// }
-// console.log(lowScore);
-
-
-//lowScore = JSON.parse(lowScore);
+var name= localStorage.getItem("name");
+if (name === null) {
+    lowScore = [""];
+}
 
 //add timer *can i use js for specific placements?
 var time = 15 * questions.length - 1; //15 seconds per question
@@ -57,7 +51,6 @@ var counter = 0;
 var numCorrect = 0;
 
 countDown.addEventListener("click", function () {
-    //var countDown = function(time) {
     var timeInterval = setInterval(function () {
         if (time <= 0) {
             clearInterval(timeInterval);
@@ -65,7 +58,6 @@ countDown.addEventListener("click", function () {
         } else {
             timer.textContent = "Time: " + time;
             time--;
-            //console.log(timeLeft);
         }
     }, 1000);
     questionDisplay(counter, questions);
@@ -76,8 +68,6 @@ var taskButtonHandler = function (event) {
     //console.log(targetEl);
 
     if (targetEl.matches("#start-btn")) {
-        //countDown(time);
-        //questionDisplay(questions);
         time = 15 * questions.length;
         countDown.className = "gone";
     } else {
@@ -90,7 +80,6 @@ var questionDisplay = function (counter, questions) {
     remove.innerHTML = "";
     listArea.innerHTML = "";
 
-    //for ( var i = 0; i < questions.length; i++){
     question.textContent = questions[counter].head;
     answers = questions[counter].options;
 
@@ -107,7 +96,6 @@ var questionDisplay = function (counter, questions) {
 var check = function (event) {
     var targetEl = event.target;
     correctIncorrect.innerHTML = "";
-    //console.log(targetEl.innerText);
 
     if (targetEl.innerText == questions[counter].answer) {
         var congrats = document.createElement("div");
@@ -144,7 +132,6 @@ var endGame = function () {
         highScore = score;
         localStorage.setItem("highscore", highScore);
         localStorage.setItem("name",ans);
-        //localStorage.setItem("highname", ans);
     }
     else{
         var ans = window.prompt("You did not beat the High Score.\nPlease Enter Your Initials");
